7abd812db72ef50572b6fa99fdea638e1d9c210b
Chris Jones <chrisj@q4inc.com>
2018-02-08 15:27:45 -0500

    Initial commit
    Just getting things working.

---
+++ Collector.Api/Function.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+namespace Collector.Api
+{
+    public class Collector
+    {
+        public Collector()
+        {
+
+        }
+
+        public APIGatewayProxyResponse GetA(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            return new APIGatewayProxyResponse
+            {
+                StatusCode = 200,
+                Headers = null,
+                Body = "Hi"
+            };
+        }
+
+        public APIGatewayProxyResponse Save(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            return new APIGatewayProxyResponse
+            {
+                StatusCode = 200,
+                Headers = null,
+                Body = "Hi"
+            };
+        }
+    }
+}
