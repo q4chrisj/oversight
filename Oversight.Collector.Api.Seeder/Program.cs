4c5523679a01789cd327e53e50d528454272e72f
Chris Jones <chrisj@q4inc.com>
2018-02-09 21:41:22 -0500

    Small improvements to test console app
    Signed-off-by: Chris Jones <chrisj@q4inc.com>

--- Oversight.Collector.Api.Seeder/Program.cs
+++ Oversight.Collector.Api.Seeder/Program.cs
@@ -11,22 +11,31 @@ namespace Oversight.Collector.Api.Seeder
     {
         static void Main(string[] args)
         {
-            for(var i = 0; i < 100; i++) {
-                var status = new OversightStatus {
-                    ServerName = "rc-cache-10",
+            int[] serverIds = new int[] { 10, 12, 14, 15, 16, 17 };
+            Random random = new Random();
+
+            for (var i = 0; i < 100; i++)
+            {
+                int randServerId = serverIds[random.Next(serverIds.Length)];
+
+                var status = new OversightStatus
+                {
+                    ServerName = "rc-cache-" + randServerId,
                     ServiceName = "couchbase",
                     Status = "Up",
                     Timestamp = DateTime.Now
                 };
 
                 string req = JsonConvert.SerializeObject(status);
-            
-                using (var client = new HttpClient()) { 
+
+                using (var client = new HttpClient())
+                {
                     client.BaseAddress = new Uri("https://tnr2n5s4x3.execute-api.us-east-1.amazonaws.com/dev/");
 
                     HttpResponseMessage response = client.PostAsync("v1/save", new StringContent(req, Encoding.Default, "application/json")).Result;
                     Console.WriteLine(response.StatusCode);
-                    if (!response.IsSuccessStatusCode) {
+                    if (!response.IsSuccessStatusCode)
+                    {
                         Console.WriteLine("There was an error");
                     }
                 }
