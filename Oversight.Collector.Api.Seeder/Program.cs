4645ef7dd473c8caa0631fb238702d1f6642e920
Chris Jones <chrisj@q4inc.com>
2018-02-26 15:41:51 -0500

    Working better
    Signed-off-by: Chris Jones <chrisj@q4inc.com>

--- Oversight.Collector.Api.Seeder/Program.cs
+++ Oversight.Collector.Api.Seeder/Program.cs
@@ -1,23 +1,21 @@
-﻿using System;
-using System.Net.Http;
-using System.Threading;
-using System.Text;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using Oversight.Collector.Model;
 using Splunk.Logging;
+using System;
+using System.Threading;
 
 namespace Oversight.Collector.Api.Seeder
 {
     class Program
     {
-        private static string _collectorApiEndpoint = "https://tnr2n5s4x3.execute-api.us-east-1.amazonaws.com/dev/";
-        private static string _splunkCollectorEndpoint = "https://http-inputs-q4inc.splunkcloud.com";
+        //private static string _collectorApiEndpoint = "https://tnr2n5s4x3.execute-api.us-east-1.amazonaws.com/dev/";
+        //private static string _splunkCollectorEndpoint = "https://http-inputs-q4inc.splunkcloud.com";
         
         static void Main(string[] args)
         {
             int[] serverIds = new int[] { 10, 12, 14, 15, 16, 17 };
             Random random = new Random();
-            var middleware = new HttpEventCollectorResendMiddleware(100);
+            var middleware = new HttpEventCollectorResendMiddleware(3);
             var sender = new HttpEventCollectorSender(
                 new Uri("https://http-inputs-q4inc.splunkcloud.com/services/collector/event/"),
                 "2550B4EC-5A5F-449F-B6AC-905DCAC57BAD",
@@ -43,7 +41,17 @@ namespace Oversight.Collector.Api.Seeder
                 };
 
                 string req = JsonConvert.SerializeObject(status);
-                sender.Send(Guid.NewGuid().ToString(), "INFO", null, req);
+
+                Console.WriteLine("Sending data to splunk");
+
+                try
+                {
+                    sender.Send(null, null, null, req.Replace("\\", ""));
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
                 
                 Thread.Sleep(30000);
             }
